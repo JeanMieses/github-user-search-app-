229fff18c4a9c4a571796fa1503ba8e84c2d925d
JeanMieses <araujo0275@gmail.com>
2021-09-04 18:10:05 -0400

    layouts fixes and refactoring

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,71 +1,41 @@
-window.onload = async function () {
+window.onload = async () => {
   const data = await axios.get(`https://api.github.com/users/JeanMieses`)
     .then(data => {
       return data.data;
     })
-
-    document.querySelector('.profile-img').src = data.avatar_url;
-
-    document.querySelector('.name').innerText = data.name;
-    document.querySelector('.username').innerText = `@${data.login}`;
-    document.querySelector('.joined-in').innerText = data.created_at;
-    const bio = data.bio || 'This profile has no bio';
-    document.querySelector('.bio').innerText = bio;
-
-    document.querySelector('.repos').innerText = data.public_repos;
-    document.querySelector('.followers').innerText = data.followers;
-    document.querySelector('.following').innerText = data.following;
-
-    const location = data.location || 'Somewhere far away';
-    document.querySelector('.location').innerText = location;
-
-    const blog = data.blog || 'Not Blog Available'
-    document.querySelector('.github-blog').innerText = blog;
-    document.querySelector('.github-blog').href = blog;
-
-    const twitter = `@${data.twitter_username}` || 'Not Available';
-    document.querySelector('.twitter-username').innerText = twitter;
-
-    const company = data.company || 'Workplace Not Available';
-    document.querySelector('.company').innerText = company;
+    displayUserData(data);
 }
-
 
 document.querySelector('form').addEventListener('submit', async (e) => {
   e.preventDefault();
-
   const data = await axios.get(`https://api.github.com/users/${e.target[0].value}`)
-    .then(data => {
-      return data.data;
-    })
+    .then(data => { return data.data; })
 
-    console.log(data);
-
-    document.querySelector('.profile-img').src = data.avatar_url;
-
-    document.querySelector('.name').innerText = data.name;
-    document.querySelector('.username').innerText = `@${data.login}`;
-    document.querySelector('.joined-in').innerText = data.created_at;
-    const bio = data.bio || 'This profile has no bio';
-    document.querySelector('.bio').innerText = bio;
-
-    document.querySelector('.repos').innerText = data.public_repos;
-    document.querySelector('.followers').innerText = data.followers;
-    document.querySelector('.following').innerText = data.following;
-
-    const location = data.location || 'Somewhere far away';
-    document.querySelector('.location').innerText = location;
-
-    const blog = data.blog || 'Not Blog Available'
-    document.querySelector('.github-blog').innerText = blog;
-    document.querySelector('.github-blog').href = blog;
-
-    const twitter = `@${data.twitter_username}` || 'Not Available';
-    console.log(data.twitter_username);
-    document.querySelector('.twitter-username').innerText = twitter;
-
-    const company = data.company || 'Workplace Not Available';
-    document.querySelector('.company').innerText = company;
-
-  e.target[0].value = '';
+    displayUserData(data);
+    e.target[0].value = '';
 })
+
+const displayUserData = (data) => {
+  document.querySelector('.profile-img').src = data.avatar_url;
+  document.querySelector('.name').innerText = data.name;
+  document.querySelector('.username').innerText = `@${data.login}`;
+
+  document.querySelector('.joined-in').innerText = dateFormat(data.created_at);
+  document.querySelector('.bio').innerText = data.bio || 'This profile has no bio';;
+
+  document.querySelector('.repos').innerText = data.public_repos;
+  document.querySelector('.followers').innerText = data.followers;
+  document.querySelector('.following').innerText = data.following;
+
+  document.querySelector('.location').innerText = data.location || 'Somewhere far away'
+  document.querySelector('.github-blog').innerText = data.blog || 'Not Blog Available';
+  document.querySelector('.github-blog').href = data.blog? `https://${data.blog}` : '#'
+  document.querySelector('.twitter-username').innerText = `@${data.twitter_username}` || 'Not Available';
+  document.querySelector('.company').innerText = data.company || 'Workplace Not Available';
+}
+
+const dateFormat = (str) => {
+  const year = new Date(Date.parse(str)).getFullYear();
+  const month = new Date(str).toLocaleString('ENG', {month: 'long'});
+  return `Joined ${month}, ${year}`
+}
