05e1eef6ffdba89cc4f69bbbdef09682506601a2
JeanMieses <araujo0275@gmail.com>
2021-09-05 23:35:19 -0400

    adding dark and light mode functionality

--- assets/javascript/index.js
+++ assets/javascript/index.js
@@ -1,3 +1,17 @@
+const bio = document.querySelector('.bio');
+const joined = document.querySelector('.joined-in');
+const followsFollwers = document.querySelector('.repos-followers-following');
+const repo = document.querySelector('.repos');
+const followers = document.querySelector('.followers');
+const following = document.querySelector('.following');
+const location1 = document.querySelector('.location');
+const blog = document.querySelector('.github-blog');
+const twitter_username = document.querySelector('.twitter-username');
+const company = document.querySelector('.company');
+const username = document.querySelector('.username');
+const name = document.querySelector('.name');
+
+// onload, we going to display my github information... Follow me guys!
 window.onload = async () => {
   const data = await axios.get(`https://api.github.com/users/JeanMieses`)
     .then(data => {
@@ -5,6 +19,29 @@ window.onload = async () => {
     })
     displayUserData(data);
 }
+
+// this toggle between light and dark mode
+document.querySelector('.toggler').addEventListener('click', (e) => {
+  document.querySelector('h1').classList.toggle('h1-lightmode');
+  document.querySelector('.light-mode-text-toggle').classList.toggle('h1-lightmode');
+  document.querySelector('body').classList.toggle('light-background');
+  document.querySelector('.user-card').classList.toggle('user-card-lightmode');
+  name.classList.toggle('h1-lightmode');
+  bio.classList.toggle('bio-lightmode');
+  joined.classList.toggle('joined-ing-lightmode');
+  followsFollwers.classList.toggle('repos-followers-following-lightmode');
+  repo.classList.toggle('text-color-lightmode');
+  followers.classList.toggle('text-color-lightmode');
+  following.classList.toggle('text-color-lightmode');
+
+  blog.classList.toggle('bio-lightmode');
+  location1.classList.toggle('bio-lightmode');
+  twitter_username.classList.toggle('bio-lightmode');
+  company.classList.toggle('bio-lightmode');
+  document.querySelector('#search-user').classList.toggle('input-lightmode');
+  document.querySelector('.form').classList.toggle('input-lightmode');
+})
+
 
 document.querySelector('form').addEventListener('submit', async (e) => {
   e.preventDefault();
@@ -15,23 +52,24 @@ document.querySelector('form').addEventListener('submit', async (e) => {
     e.target[0].value = '';
 })
 
+// this function display the data for the user when we clikc in search
 const displayUserData = (data) => {
   document.querySelector('.profile-img').src = data.avatar_url;
-  document.querySelector('.name').innerText = data.name;
-  document.querySelector('.username').innerText = `@${data.login}`;
+  name.innerText = data.name;
+  username.innerText = `@${data.login}`;
 
-  document.querySelector('.joined-in').innerText = dateFormat(data.created_at);
-  document.querySelector('.bio').innerText = data.bio || 'This profile has no bio';;
+  joined.innerText = dateFormat(data.created_at);
+  bio.innerText = data.bio || 'This profile has no bio';;
 
-  document.querySelector('.repos').innerText = data.public_repos;
-  document.querySelector('.followers').innerText = data.followers;
-  document.querySelector('.following').innerText = data.following;
+  repo.innerText = data.public_repos;
+  followers.innerText = data.followers;
+  following.innerText = data.following;
 
-  document.querySelector('.location').innerText = data.location || 'Not Location'
-  document.querySelector('.github-blog').innerText = data.blog || 'Not Blog';
-  document.querySelector('.github-blog').href = data.blog? `https://${data.blog}` : '#'
-  document.querySelector('.twitter-username').innerText = (data.twitter_username===null) ? 'Not Twitter' : `@${data.twitter_username}`
-  document.querySelector('.company').innerText = data.company || 'Not Workplace';
+  location1.innerText = data.location || 'Not Location'
+  blog.innerText = data.blog || 'Not Blog';
+  blog.href = data.blog? `https://${data.blog}` : '#'
+  twitter_username.innerText = (data.twitter_username===null) ? 'Not Twitter' : `@${data.twitter_username}`
+  company.innerText = data.company || 'Not Workplace';
 }
 
 const dateFormat = (str) => {
